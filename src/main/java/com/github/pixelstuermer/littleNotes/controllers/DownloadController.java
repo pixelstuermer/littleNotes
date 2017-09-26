865cd4a71f5a3de1e415748ff4e8d716740e68d0
pixelstuermer <philipp.montesano@hs-furtwangen.de>
2017-09-26 19:38:45 +0200

    Changed download method

--- src/main/java/com/github/pixelstuermer/littleNotes/controllers/DownloadController.java
+++ src/main/java/com/github/pixelstuermer/littleNotes/controllers/DownloadController.java
@@ -1,17 +1,20 @@
 package com.github.pixelstuermer.littleNotes.controllers;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.pixelstuermer.littleNotes.models.Collection;
-import com.mongodb.BasicDBList;
+import com.github.pixelstuermer.littleNotes.models.NoteEntry;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCursor;
 
@@ -31,7 +34,7 @@ public class DownloadController {
 
    @RequestMapping( method = RequestMethod.GET, value = "/all/json", produces = "application/json" )
    @ApiOperation( value = "Downloads all note documents as JSON" )
-   public void downloadAllJson( HttpServletResponse response ) {
+   public ResponseEntity<List<NoteEntry>> downloadAllJson( HttpServletResponse response ) {
       // set response headers
       String filename = "all-notes-" + new Date().getTime();
       response.addHeader( "Content-disposition", "attachment;filename=" + filename + ".json" );
@@ -39,20 +42,15 @@ public class DownloadController {
 
       // run mongo query
       DBCursor cursor = mongoTemplate.getCollection( collection.getCollectionName() ).find();
-      BasicDBList docsList = new BasicDBList();
+      List<NoteEntry> entryList = new ArrayList<>();
       while ( cursor.hasNext() ) {
          BasicDBObject document = (BasicDBObject) cursor.next();
-         docsList.add( document );
+         NoteEntry noteEntry = mongoTemplate.getConverter().read( NoteEntry.class, document );
+         entryList.add( noteEntry );
       }
 
-      // write
-      try {
-         response.getOutputStream().write( docsList.toString().getBytes() );
-         response.getOutputStream().flush();
-      }
-      catch ( Exception e ) {
-         e.printStackTrace();
-      }
+      // serve download
+      return ResponseEntity.ok().body( entryList );
    }
 
 }
