161acdfdb864f5f91f3f01756748fdc0a63c6837
pixelstuermer <philipp.montesano@hs-furtwangen.de>
2017-10-02 22:40:44 +0200

    Removed unnecessary logger

--- src/main/java/com/github/pixelstuermer/littleNotes/application/ApplicationStarter.java
+++ src/main/java/com/github/pixelstuermer/littleNotes/application/ApplicationStarter.java
@@ -1,7 +1,5 @@
 package com.github.pixelstuermer.littleNotes.application;
 
-import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,9 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan( basePackages = "com.github.pixelstuermer.littleNotes" )
 public class ApplicationStarter {
-
-   @Autowired
-   Logger logger;
 
    public static void main( String[] args ) {
       SpringApplication.run( ApplicationStarter.class, args );
